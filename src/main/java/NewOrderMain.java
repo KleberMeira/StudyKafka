be9fb6983d764da099e7d5753d3f7099b311d67f
Kleber <klebermeira4@gmail.com>
2021-03-23 22:11:45 -0400

    log e email service

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -1,5 +1,6 @@
 //package br.com.kleber.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -9,6 +10,8 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+//Responsável por processar uma nova ordem
+
 public class NewOrderMain {
 
     public static void main (String[] args) throws ExecutionException, InterruptedException {
@@ -16,15 +19,19 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "123123,13124124,542525";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null){
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
 
-            System.out.println("sucesso enviando" + data.topic() + ":::partition" +data.partition() +"/offset" + data.offset() + "/timestam" + data.timestamp());
+            System.out.println("sucesso enviando" + data.topic() + ":::partition" + data.partition() + "/offset" + data.offset() + "/timestam" + data.timestamp());
 
-        }).get();//espera obter a resposta do send .get()
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();//espera obter a resposta do send .get()
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
